ec42c668ca6f7f042d1aacfee9ee2ae3c68a1bad
unknown <mario.akhras@gmail.com>
2020-05-16 17:46:23 -0500

    organized the functions

--- server.js
+++ server.js
@@ -4,58 +4,46 @@ var ShareDB = require('sharedb');
 var richText = require('rich-text');
 var WebSocket = require('ws');
 var WebSocketJSONStream = require('@teamwork/websocket-json-stream');
-var firebase = require('firebase')
 
 
 ShareDB.types.register(richText.type);
-var backend = new ShareDB();
 
 startServer()
 
-function saveScript () {
-  var saveScriptData = firebase.functions().httpsCallable('saveScriptData'); 
+// function getScript() {
+//   let connection = backend.connect();
+//   let doc = connection.get('temporada1', 'episodio1');
+//   doc.fetch(function(err) {
+//     if (err) throw err;
+//     if (doc.type === null) {
+//       // doc.create([{insert: '\n \n ESC'}, {attributes: {scriptScene: {type: 'script'}}}, {insert: '\n'}], 'rich-text', callback);
+//       doc.create([{insert: 'ESCENA'}], 'rich-text');
+//       console.log('CREATED SCRIPT..--..--..--..--..');
+//       // return;
+//     }
+//   });
+// }
 
-  saveScriptData().then(function(resp) {
-    console.log('EXITO! ', resp.data.result);
-  }).catch(function(error) {
-    console.log('ERROR CALLING CLOUD FUNCTION... : ', error);
-  });
-}
-
-function getScript(callback) {
-  console.log('GETTING SCRIPT..--..--..--..--..');
-  var connection = backend.connect();
-  var doc = connection.get('temporada1', 'episodio1');
-  doc.fetch(function(err) {
-    if (err) throw err;
-    if (doc.type === null) {
-      // doc.create([{insert: '\n \n ESC'}, {attributes: {scriptScene: {type: 'script'}}}, {insert: '\n'}], 'rich-text', callback);
-      doc.create([{insert: 'ESCENA'}], 'rich-text', callback);
-      return;
-    }
-  });
-}
-
-function getIdeas (request, response) {
-  console.log('GETTING IDEAS..//..//..//..//..');
-  let connection = backend.connect()
-  let doc = connection.get('ideas', 'lluvia')
-  doc.fetch(err=>{
-    if (err) throw err
-    if (doc.type===null) {
-      console.log('Oops made a doc: ', doc);
-      doc.create({list: [{idea: 'Primera idea...', order: 0}]})
-      return;
-    }
-    getIdeas()
-  })
-}
+// function getIdeas (request, response) {
+//   let connection = backend.connect()
+//   let doc = connection.get('ideas', 'lluvia')
+//   doc.fetch(err=>{
+//     if (err) throw err
+//     if (doc.type===null) {
+//       console.log('Oops made a doc: ', doc);
+//       doc.create({list: [{idea: 'Primera idea...', order: 0}]})
+//       console.log('CREATED IDEAS..//..//..//..//..');
+//       return;
+//     }
+//   })
+// }
 
 function startServer() {
-  var app = express();
+  let app = express();
   app.use(express.static('static'));
   app.use(express.static('node_modules/quill/dist'));
-  var server = http.createServer(app);
+  let server = http.createServer(app);
+  let backend = new ShareDB();
 
   // Connect any incoming WebSocket connection to ShareDB
   var wss = new WebSocket.Server({server: server});
@@ -64,8 +52,35 @@ function startServer() {
     backend.listen(stream);
   });
 
-  getScript();
-  getIdeas();
+
+  
+  let connection = backend.connect();
+
+  let scriptDoc = connection.get('temporada1', 'episodio1');
+  scriptDoc.fetch(function(err) {
+    if (err) throw err;
+    if (scriptDoc.type === null) {
+      // scriptDoc.create([{insert: '\n \n ESC'}, {attributes: {scriptScene: {type: 'script'}}}, {insert: '\n'}], 'rich-text', callback);
+      scriptDoc.create([{insert: 'ESCENA'}], 'rich-text');
+      console.log('CREATED SCRIPT..--..--..--..--..');
+      // return;
+    }
+  });
+  
+
+  let ideasDoc = connection.get('ideas', 'lluvia');
+  ideasDoc.fetch(err=>{
+    if (err) throw err
+    if (ideasDoc.type===null) {
+      // console.log('Oops made a DOC: ', ideasDoc);
+      ideasDoc.create({list: [{idea: 'Primera idea...', order: 0}]});
+      console.log('CREATED IDEAS..//..//..//..//..');
+      return;
+    }
+  });
+
+  // getScript();
+  // getIdeas();
 
   const port = process.env.PORT || 8083
   server.listen(port);
