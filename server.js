1a3f945ac33e99505a30e5ea9ffe6eaaca2e5903
unknown <mario.akhras@gmail.com>
2020-05-15 22:20:50 -0500

    stopped using axios

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var richText = require('rich-text');
 var WebSocket = require('ws');
 var WebSocketJSONStream = require('@teamwork/websocket-json-stream');
 
+
 ShareDB.types.register(richText.type);
 var backend = new ShareDB();
+
 createDoc(startServer);
 
 // Create initial document then fire callback
 function createDoc(callback) {
   var connection = backend.connect();
-  var doc = connection.get('ideas', 'lluvia');
+  var doc = connection.get('temporada1', 'episodio1');
   doc.fetch(function(err) {
     if (err) throw err;
     if (doc.type === null) {
@@ -23,8 +25,26 @@ function createDoc(callback) {
   });
 }
 
+function getIdeas (request, response) {
+  console.log('GETTING IDEAS..//..//..//..//..');
+  let connection = backend.connect()
+  let doc = connection.get('ideas', 'lluvia')
+  doc.fetch(err=>{
+    if (err) throw err
+    if (doc.type===null) {
+      console.log('Oops made a doc: ', doc);
+      doc.create({list: [{idea: 'Primera idea...', order: 0}]})
+      return;
+    } else {
+      console.log('DOC! ', doc.data);
+    }
+  })
+  // response.send('All is well.')
+}
+
 function startServer() {
   // Create a web server to serve files and listen to WebSocket connections
+  
   var app = express();
   app.use(express.static('static'));
   app.use(express.static('node_modules/quill/dist'));
@@ -37,8 +57,10 @@ function startServer() {
     backend.listen(stream);
   });
 
-  server.listen(8080);
-  console.log('Listening on http://localhost:8080');
+  getIdeas()
+
+  server.listen(8083);
+  console.log('Listening on http://localhost:8083');
 }
 
 
